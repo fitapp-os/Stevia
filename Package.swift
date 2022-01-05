596a949fea6a326a982664520c25ca3bb8a51460
Markus Friedl <markus.friedl@bytepoets.com>
2022-01-05 13:23:21 +0100

    Update minimum iOS version

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "Stevia",
-    platforms: [.iOS(.v9), .tvOS(.v10)],
+    platforms: [.iOS(.v11), .tvOS(.v10)],
     products: [.library(name: "Stevia", targets: ["Stevia"])],
     targets: [
         .target(name: "Stevia"),
